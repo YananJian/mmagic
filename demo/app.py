b88cbb40d61e035969eeaac91beed6e6cb04d066
YananJian <jianyanan@gmail.com>
2023-09-29 07:31:27 +0000

    fix demo code

--- demo/app.py
+++ demo/app.py
@@ -8,35 +8,54 @@ from PIL import Image
 from mmagic.registry import MODELS
 from mmagic.utils import register_all_modules
 from mmengine.runner import load_checkpoint
+import torch
 
 register_all_modules()
 
 #cfg = Config.fromfile('configs/controlnet/controlnet-1xb1-fill50k_lora_inference.py')
-checkpoint_file = '/data/dreambooth-lora/iter_10000_revised.pth'
+checkpoint_file = 'karameru_10000_revised.pth'
 cfg = Config.fromfile('configs/dreambooth/dreambooth-lora-inference.py')
 dreambooth_model = MODELS.build(cfg.model)
 ckpt = load_checkpoint(dreambooth_model, checkpoint_file, map_location='cpu')
 
-dreambooth_model.to('cuda:0')
+if torch.cuda.is_available():
+  device = torch.device("cuda")
+else:
+  device = torch.device("cpu")
+
+dreambooth_model.to(device)
 dreambooth_model.eval()
 
 def predict(prompt):
     #prompt = 'A karameru cat doing weight lifting in a gym'
     seed = 20
     samples = []
-    for seed in range(10,11):
-        output_dict = dreambooth_model.infer(prompt, num_inference_steps=80, negative_prompt='colorful', seed=seed, num_images_per_prompt=2)
+    for seed in range(12, 13):
+        output_dict = dreambooth_model.infer(prompt, num_inference_steps=100, negative_prompt='colorful', seed=seed, num_images_per_prompt=2)
         samples.extend(output_dict['samples'])
     return samples
 
+def show():
+    imgs = ['IMG_5371.jpg', 'IMG_5381.jpg', 'IMG_5388.jpg', 'IMG_5408.jpg']
+    return imgs
 
-with gr.Blocks() as demo:
+
+with gr.Blocks(title='Doodle Artist') as demo:
+    gr.Markdown(
+    """
+    <h1 style="text-align: center;">Doodle Artist Helper</h1>
+    """)
     with gr.Column(variant="panel"):
+        with gr.Row():
+            t_btn = gr.Button("Show Samples of Training Images", scale=0)
+            t_gallery = gr.Gallery(
+                label="Samples of Training Images", show_label=False, elem_id="gallery"
+                , columns=[4], rows=[1], object_fit="contain", height="auto")
         with gr.Row():
             text = gr.Textbox(
                 label="Enter your prompt",
                 max_lines=1,
-                placeholder="A karameru cat ...",
+                value="A karameru cat dancing on the floor",
                 container=False,
             )
             btn = gr.Button("Generate image", scale=0)
@@ -47,18 +66,7 @@ with gr.Blocks() as demo:
 
     btn.click(predict, text, gallery)
 
+    t_btn.click(show, None, t_gallery)
 
-
-#demo = gr.Interface(sepia, gr.Image(shape=(200, 200)), "image")
-'''
-demo = gr.Interface(fn=predict,
-             inputs=gr.Textbox(lines=2, placeholder="A karameru cat ..."),
-             outputs=["image", "image"],
-             examples=["A karameru cat doing weight lifting in a gym"])
-gallery = gr.Gallery(
-            label="Generated images", show_label=False, elem_id="gallery"
-        , columns=[2], rows=[2], object_fit="contain", height="auto")
-
-'''
 if __name__ == "__main__":
-    demo.launch(server_name='0.0.0.0', server_port=7006)
+    demo.launch(server_name='0.0.0.0', server_port=7806)
