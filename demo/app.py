dd297490e0691ba40c8c7fe23b05db83d8f41323
YananJian <jianyanan@gmail.com>
2023-09-30 05:27:51 +0000

    change port

--- demo/app.py
+++ demo/app.py
@@ -69,4 +69,4 @@ with gr.Blocks(title='Doodle Artist') as demo:
     t_btn.click(show, None, t_gallery)
 
 if __name__ == "__main__":
-    demo.launch(server_name='0.0.0.0', server_port=7806)
+    demo.launch(server_name='0.0.0.0', server_port=7860)
